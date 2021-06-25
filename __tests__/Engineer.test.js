297b1bcdb906d1ad029af25ba83cb2769fa35a1f
Twyll <twyllblackleaf@gmail.com>
2021-06-25 16:29:16 -0400

    finish Engineer class

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,19 +1,21 @@
 const Engineer = require("../lib/Engineer");
 
 test("creates an Engineer object", () => {
-    const xyzzy = new Engineer("Xyzzy", 1, "xyzzy@foo.bar");
+    const xyzzy = new Engineer("Xyzzy", 1, "xyzzy@foo.bar", "xyzzygithub");
 
     expect(xyzzy.name).toBe("Xyzzy");
     expect(xyzzy.id).toBe(1);
     expect(xyzzy.email).toBe("xyzzy@foo.bar");
+    expect(xyzzy.github).toBe("xyzzygithub");
     expect(xyzzy.role).toBe("Engineer");
 });
 
 test("gets parameters of Engineer object", () => {
-    const xyzzy = new Engineer("Xyzzy", 1, "xyzzy@foo.bar");
+    const xyzzy = new Engineer("Xyzzy", 1, "xyzzy@foo.bar", "xyzzygithub");
 
     expect(xyzzy.getName()).toBe("Xyzzy");
     expect(xyzzy.getId()).toBe(1);
     expect(xyzzy.getEmail()).toBe("xyzzy@foo.bar");
+    expect(xyzzy.getGithub()).toBe("xyzzygithub");
     expect(xyzzy.getRole()).toBe("Engineer");
 });
