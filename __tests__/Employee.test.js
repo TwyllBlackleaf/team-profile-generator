7d8ab5b6e5ff80068f7bc194ecc169b2abb5ce4e
Twyll <twyllblackleaf@gmail.com>
2021-06-25 16:15:24 -0400

    set up Employee class

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,19 @@
+const Employee = require("../lib/Employee");
+
+test("creates an Employee object", () => {
+    const xyzzy = new Employee("Xyzzy", 1, "xyzzy@foo.bar");
+
+    expect(xyzzy.name).toBe("Xyzzy");
+    expect(xyzzy.id).toBe(1);
+    expect(xyzzy.email).toBe("xyzzy@foo.bar");
+    expect(xyzzy.role).toBe("Employee");
+});
+
+test("gets parameters of Employee", () => {
+    const xyzzy = new Employee("Xyzzy", 1, "xyzzy@foo.bar");
+
+    expect(xyzzy.getName()).toBe("Xyzzy");
+    expect(xyzzy.getId()).toBe(1);
+    expect(xyzzy.getEmail()).toBe("xyzzy@foo.bar");
+    expect(xyzzy.getRole()).toBe("Employee");
+})
