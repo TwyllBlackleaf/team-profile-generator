6c9263aa86904e7f1954b108e747c3553a0db868
Twyll <twyllblackleaf@gmail.com>
2021-06-27 22:55:38 -0400

    test for generateHTML function

--- __tests__/generateHTML.test.js
+++ __tests__/generateHTML.test.js
@@ -6,5 +6,22 @@ const generateHTML = require("../src/generateHTML");
 test("generates HTML page", () => {
     const manager = new Manager("Xyzzy", 1, "xyzzy@foo.bar", "102B");
     const engineer = [new Engineer("Foo", 2, "foo@bar.baz", "Octocat")];
-    const intern = [new Intern("Bar", 3, "bar@fizz.buzz", "Miskatonic University")]
+    const intern = [new Intern("Bar", 3, "bar@fizz.buzz", "Miskatonic University")];
+
+    const generatedSite = generateHTML(manager, engineer, intern);
+
+    expect(generatedSite).toEqual(expect.stringContaining("1"));
+    expect(generatedSite).toEqual(expect.stringContaining("Xyzzy"));
+    expect(generatedSite).toEqual(expect.stringContaining("xyzzy@foo.bar"));
+    expect(generatedSite).toEqual(expect.stringContaining("102B"));
+
+    expect(generatedSite).toEqual(expect.stringContaining("2"));
+    expect(generatedSite).toEqual(expect.stringContaining("Foo"));
+    expect(generatedSite).toEqual(expect.stringContaining("foo@bar.baz"));
+    expect(generatedSite).toEqual(expect.stringContaining("Octocat"));
+
+    expect(generatedSite).toEqual(expect.stringContaining("3"));
+    expect(generatedSite).toEqual(expect.stringContaining("Bar"));
+    expect(generatedSite).toEqual(expect.stringContaining("bar@fizz.buzz"));
+    expect(generatedSite).toEqual(expect.stringContaining("Miskatonic University"));
 })
